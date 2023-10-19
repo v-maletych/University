a7d1c04e1ef2fe51172901f2a91f6aa58df4d97d
Vitalii Maletych <124867918+v-maletych@users.noreply.github.com>
2023-10-19 10:22:36 +0300

    Create server.py

---
+++ messenger_for_localhost/server.py
@@ -0,0 +1,35 @@
+from socket import *
+from threading import *
+
+clients = set()
+
+hostSocket = socket(AF_INET, SOCK_STREAM)
+hostSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR,1)
+
+hostIp = "127.0.0.1"
+portNumber = 7500
+hostSocket.bind((hostIp, portNumber))
+hostSocket.listen()
+print ("Waiting for connection...")
+
+def clientThread(clientSocket, clientAddress):
+    while True:
+        message = clientSocket.recv(1024).decode("utf-8")
+        print(clientAddress[0] + ":" + str(clientAddress[1]) +": "+ message)
+        for client in clients:
+            if client is not clientSocket:
+                client.send((clientAddress[0] + ":" + str(clientAddress[1]) +": "+ message).encode("utf-8"))
+
+        if not message:
+            clients.remove(clientSocket)
+            print(clientAddress[0] + ":" + str(clientAddress[1]) +" disconnected...")
+            break
+
+    clientSocket.close()
+
+while True:
+    clientSocket, clientAddress = hostSocket.accept()
+    clients.add(clientSocket)
+    print ("Connection established with: ", clientAddress[0] + ":" + str(clientAddress[1]))
+    thread = Thread(target=clientThread, args=(clientSocket, clientAddress, ))
+    thread.start()
