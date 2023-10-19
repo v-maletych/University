394bfa0e3dd2eec287c276f6dfd5cbe70cf7e572
Vitalii Maletych <124867918+v-maletych@users.noreply.github.com>
2023-10-19 12:39:08 +0300

    Update simple_calc_python.py

--- simple_calc_python.py
+++ simple_calc_python.py
@@ -1,19 +1,20 @@
-ask = input("\nSimple calculator…\n\nChoose the action (1-4): \n 1. +\n 2. -\n 3. *\n 4. /\nYour choise: ")
-if ask == "1":
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    print("Result:", num1 + num2)
-elif ask == "2":
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    print("Result:", num1 - num2)
-elif ask == "3":
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    print("Result:", num1 * num2)
-elif ask == "4":
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    print("Result:", num1 / num2)
-else:
-    print("You entered the wrong action! Restart the program and try again...")
+while True:
+    ask = input("\nSimple calculator…\n\nChoose the action (1-4): \n 1. +\n 2. -\n 3. *\n 4. /\nYour choise: ")
+    if ask == "1":
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        print("Result:", num1 + num2)
+    elif ask == "2":
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        print("Result:", num1 - num2)
+    elif ask == "3":
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        print("Result:", num1 * num2)
+    elif ask == "4":
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        print("Result:", num1 / num2)
+    else:
+        print("You entered the wrong action! Restart the program and try again...")
